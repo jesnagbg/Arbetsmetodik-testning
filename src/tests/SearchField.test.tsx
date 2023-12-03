90d6bc882a0c1354dd0e9e1ee45168eaf7d795a5
Jenny Pettersson <j.m.pettersson@hotmail.se>
2023-12-03 20:16:22 +0100

    better search tests

--- src/tests/SearchField.test.tsx
+++ src/tests/SearchField.test.tsx
@@ -1,51 +1,49 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { beforeEach, describe, expect, it } from 'vitest';
 import SearchField from '../components/SearchField';
 
 describe('SearchField Component', () => {
-  it('allows user to enter a word', () => {
+  const user = userEvent.setup();
+
+  let inputElement: HTMLInputElement;
+  let buttonElement: HTMLButtonElement;
+
+  beforeEach(() => {
     render(<SearchField handleSearch={() => {}} />);
-    const input = screen.getByPlaceholderText(
+    inputElement = screen.getByPlaceholderText(
       'Enter a word'
     ) as HTMLInputElement;
-    fireEvent.change(input, { target: { value: 'test' } });
-    expect(input.value).toBe('test');
+    buttonElement = screen.getByRole('button');
   });
 
-  it('clears the input field when the search button is clicked', () => {
-    render(<SearchField handleSearch={() => {}} />);
-    const inputElement = screen.getByPlaceholderText(
-      'Enter a word'
-    ) as HTMLInputElement;
-    const buttonElement = screen.getByRole('button');
+  it('allows user to enter a word', async () => {
+    await user.type(inputElement, 'test');
+    expect(inputElement.value).toBe('test');
+  });
 
-    fireEvent.change(inputElement, { target: { value: 'testword' } });
-    fireEvent.click(buttonElement);
-
+  it('clears the input field when the search button is clicked', async () => {
+    await user.type(inputElement, 'testword');
+    await user.click(buttonElement);
     expect(inputElement.value).toBe('');
   });
 
-  it('displays an error message if the search button is clicked without entering a word', () => {
-    render(<SearchField handleSearch={() => {}} />);
-    const buttonElement = screen.getByRole('button');
-
-    fireEvent.click(buttonElement);
-
+  it('displays an error message if the search button is clicked without entering a word', async () => {
+    await user.click(buttonElement);
     const errorMessage = screen.getByText('Please enter a word');
     expect(errorMessage).toBeInTheDocument();
   });
 
-  it('clears the error message when a valid word is entered after an error', () => {
-    render(<SearchField handleSearch={() => {}} />);
-    const buttonElement = screen.getByRole('button');
-    fireEvent.click(buttonElement); // Trigger error
+  it('clears the error message when a valid word is entered after an error', async () => {
+    await user.click(buttonElement); // Trigger error
 
-    const inputElement = screen.getByPlaceholderText(
-      'Enter a word'
-    ) as HTMLInputElement;
-    fireEvent.change(inputElement, { target: { value: 'validword' } });
+    let errorMessage = screen.queryByText('Please enter a word');
+    expect(errorMessage).toBeInTheDocument();
 
-    const errorMessage = screen.queryByText('Please enter a word');
+    await user.type(inputElement, 'testword'); // Enter a valid word
+    await userEvent.click(buttonElement);
+
+    errorMessage = screen.queryByText('Please enter a word');
     expect(errorMessage).not.toBeInTheDocument();
   });
 });
